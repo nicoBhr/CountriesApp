2fdb34009ba11a3d5226e4816ace61f9d619b041
nicoBhr <nicolasbecher@hotmail.com>
2022-06-06 20:45:18 -0300

    Create component routes

--- PI-Countries-main/client/src/App.js
+++ PI-Countries-main/client/src/App.js
@@ -1,9 +1,28 @@
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import LandingPage from './components/LandingPage/LandingPage.jsx';
+import Home from './components/Home/Home.jsx';
+import Country from './components/Country/Country.jsx';
+import Activity from './components/Activity/Activity.jsx';
+import NotFound from './components/NotFound/NotFound.jsx';
+
 
 function App() {
   return (
     <div className="App">
-      <h1>Henry Countries</h1>
+      <BrowserRouter>
+        <Routes>
+        <Route path="/" element={<LandingPage />} />
+
+        <Route path="/home" element={<Home />} />
+
+        <Route path="/country/:id" element={<Country />} />
+
+        <Route path="/activity" element={<Activity />} />
+
+        <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ PI-Countries-main/client/src/components/LandingPage/LandingPage.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+// import { Link } from 'react-router-dom';
+
+export default function LandingPage() {
+    return (
+        <div>Landing Page</div>
+    )
+}
